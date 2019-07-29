8024bb1ad70cf30ad1ba82d05eebc2cf5b5d371c
stakx <stakx@eml.cc>
2019-07-29 21:45:25 +0200

    Initial commit

---
+++ VSTestProgressIndicatorFloodRepro/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(4)]
+    [TestCase(5)]
+    [TestCase(6)]
+    [TestCase(7)]
+    [TestCase(8)]
+    [TestCase(9)]
+    public void Test(int i)
+    {
+        Thread.Sleep(1000);
+    }
+}
